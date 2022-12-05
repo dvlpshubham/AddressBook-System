3e37c7aecd5d757e2ba6b3023c5a3e12076e55dd
Shubham Raj <shubhamaryan805@gmail.com>
2022-12-05 22:06:25 +0530

    Added : Ability to add Multiple contacts in address book

--- src/com/addressbook/Contacts.java
+++ src/com/addressbook/Contacts.java
@@ -1,100 +1,67 @@
 package com.addressbook;
 
 public class Contacts {
-    private String firstName, lastName, address, city, state, email, zip, phoneNumber;
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String city;
+    private String state;
+    private int zip;
+    private String phoneNumber;
+    private String emailId;
 
-    public Contacts(String firstName, String lastName, String address, String city, String state, String email, String zip, String phoneNumber) {
+    Contacts(String firstName, String lastName, String address, String city, String state, int zip, String phoneNumber,
+             String emailId) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.address = address;
         this.city = city;
         this.state = state;
-        this.email = email;
         this.zip = zip;
         this.phoneNumber = phoneNumber;
+        this.emailId = emailId;
     }
 
     public String getFirstName() {
         return firstName;
     }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
     public String getLastName() {
         return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
     }
 
     public String getAddress() {
         return address;
     }
 
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
     public String getCity() {
         return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
     }
 
     public String getState() {
         return state;
     }
 
-    public void setState(String state) {
-        this.state = state;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getZip() {
+    public int getZip() {
         return zip;
-    }
-
-    public void setZip(String zip) {
-        this.zip = zip;
     }
 
     public String getPhoneNumber() {
         return phoneNumber;
     }
 
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
+    public String getEmailId() {
+        return emailId;
     }
 
     @Override
     public String toString() {
-        return "Contacts{" +
-                "firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", address='" + address + '\'' +
-                ", city='" + city + '\'' +
-                ", state='" + state + '\'' +
-                ", email='" + email + '\'' +
-                ", zip='" + zip + '\'' +
-                ", phoneNumber='" + phoneNumber + '\'' +
-                '}';
+        return "PersonInformation : \n" + " firstName = " + firstName + "\n" + " lastName = " + lastName + "\n"
+                + " address =" + address + "\n" + " city =" + city + "\n" + " state =" + state + "\n" + " zip =" + zip
+                + "\n" + " phoneNumber =" + phoneNumber + "\n" + " emailId =" + emailId;
     }
 
-
 }
-
 
 
 
---
+++ src/com/addressbook/Main.java
@@ -0,0 +1,9 @@
+package com.addressbook;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the address book system");
+        AddressBook addressBook = new AddressBook();
+        addressBook.addMultiplePerson();
+    }
+}

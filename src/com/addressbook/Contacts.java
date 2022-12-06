b1c21b3a666ec800f57f87a72e82552e725ccb9a
Shubham Raj <shubhamaryan805@gmail.com>
2022-12-06 20:26:32 +0530

    Added : multiple contact

--- src/com/addressbook/Contacts.java
+++ src/com/addressbook/Contacts.java
@@ -1,67 +1,90 @@
 package com.addressbook;
 
 public class Contacts {
-    private String firstName;
-    private String lastName;
-    private String address;
-    private String city;
-    private String state;
+    private String firstName, lastName, address, city, state, emailId;
     private int zip;
-    private String phoneNumber;
-    private String emailId;
+    private long phoneNumber;
 
-    Contacts(String firstName, String lastName, String address, String city, String state, int zip, String phoneNumber,
-             String emailId) {
+    //Used constructor to store parameters
+    public Contacts (String firstName, String lastName, String address, String city, String state, String emailId, int zip, long phoneNumber){
         this.firstName = firstName;
         this.lastName = lastName;
         this.address = address;
         this.city = city;
         this.state = state;
+        this.emailId = emailId;
         this.zip = zip;
         this.phoneNumber = phoneNumber;
-        this.emailId = emailId;
     }
 
+    //Usd getters And setters to get and set contact details
     public String getFirstName() {
         return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
     }
 
     public String getLastName() {
         return lastName;
     }
 
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
     public String getAddress() {
         return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
     }
 
     public String getCity() {
         return city;
     }
 
+    public void setCity(String city) {
+        this.city = city;
+    }
+
     public String getState() {
         return state;
     }
 
-    public int getZip() {
-        return zip;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
+    public void setState(String state) {
+        this.state = state;
     }
 
     public String getEmailId() {
         return emailId;
     }
 
-    @Override
-    public String toString() {
-        return "PersonInformation : \n" + " firstName = " + firstName + "\n" + " lastName = " + lastName + "\n"
-                + " address =" + address + "\n" + " city =" + city + "\n" + " state =" + state + "\n" + " zip =" + zip
-                + "\n" + " phoneNumber =" + phoneNumber + "\n" + " emailId =" + emailId;
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
     }
 
+    public int getZip() {
+        return zip;
+    }
+
+    public void setZip(int zip) {
+        this.zip = zip;
+    }
+
+    public long getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public String toString() {
+        return ("First name: " + firstName + "\n Last name: " + lastName + "\n Address: " + address + "\n city: " + city
+                + "\n state: " +state + "\n email: " + emailId + "\n zip: " + zip + "\n phone number:" + phoneNumber );
+    }
 }
-
-
-
--- src/com/addressbook/Main.java
+++
@@ -1,9 +0,0 @@
-package com.addressbook;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Welcome to the address book system");
-        AddressBook addressBook = new AddressBook();
-        addressBook.addMultiplePerson();
-    }
-}
